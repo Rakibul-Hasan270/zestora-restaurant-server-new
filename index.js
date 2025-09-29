e2d6002ee9e521ee304b6cae938367c0581b8dd8
Rakib Hossain <rakib@example.com>
2025-09-29 17:02:04 +0600

    added menu filter, sort, reset and also searchinput apies

--- index.js
+++ index.js
@@ -24,6 +24,45 @@ async function run() {
     try {
         await client.connect();
 
+        const menuCollection = client.db("zestora_restaurant_new").collection('menu');
+
+        app.get('/menu', async (req, res) => {
+            const result = await menuCollection.find().toArray();
+            res.send(result);
+        })
+
+        // for Pagination 
+        app.get('/menuCount', async (req, res) => {
+            const filter = req.query.filter;
+            const search = req.query.search;
+
+            let query = {
+                name: { $regex: search, $options: 'i' }
+            }
+            if (filter) query.category = filter;
+            const result = await menuCollection.countDocuments(query);
+            res.send({ result });
+        })
+
+        app.get('/all-menu', async (req, res) => {
+            const page = parseInt(req.query.page) - 1;
+            const size = parseInt(req.query.size);
+            const filter = req.query.filter;
+            const sort = req.query.sort;
+            const search = req.query.search;
+
+            let option = {};
+            if (sort) option = { sort: { price: sort === 'asc' ? 1 : -1 } };
+
+            let query = {
+                name: { $regex: search, $options: 'i' }
+            }
+            if (filter) query.category = filter;
+
+            const result = await menuCollection.find(query, option).skip(page * size).limit(size).toArray();
+            res.send(result);
+        })
+
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally { }
